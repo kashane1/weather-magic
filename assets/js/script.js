4024e06d68e04cc35c4abc17c024f1be71819926
Kashane Sakhakorn Laptop <ksakhakorn@gmail.com>
2022-02-03 11:41:40 -0800

    Started with pseudo code in my index.html and just a few lines to start off the script.js. Also created all the necessary folders and files for later use. Also copied in a standard jass.css

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+
+$("#past-searches").
+
+will need to grab all lat and lon of major cities?
+or find an api that can convert city names to lat and lon
+
+function searchApi(latitude, longitude) {
+
+https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+397ffb68060871749257fde6bd6eeafb
