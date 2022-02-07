235111582923004c9fda324934d0bc3aaaf336a6
Kashane Sakhakorn Laptop <ksakhakorn@gmail.com>
2022-02-06 20:55:34 -0800

    finally got it all working. as in the api calls and the weather to show up with todays data and then the 5 day forecast. also built out the cards to work properly as well and made them look decent. now I'm getting stuck on the past searches area and having those act like buttons to call back the weather data. will resume tomorrow

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,20 @@ var resultTextEl = document.createElement('p')
 var resultContentEl = document.createElement('p')
 var xycords = [];
 
-//this is the url fetch function
+//needed to create a function to use for on click and for past search clicks
+//this selecter is only on the search input
 $("#searchSubmit").click(function() {
     cityZip = $("input").val();
     console.log(xycords);
-    
+
+    // this creates the past searches list, event listener to research them is added later
+    $("#pastSearches").prepend("<li class='btn btn-info display-flex' id='"+cityZip+"'>"+cityZip+"</li>")
+
+    findWeather(cityZip);
+});
+
+//this is the weather function that can get called by the search input as well as the past search buttons
+function findWeather(cityZip) {
     //this function get the cords for each city
     //trying a new api to get latlon
     fetch("http://api.openweathermap.org/geo/1.0/direct?q="+cityZip+"&limit=1&appid=397ffb68060871749257fde6bd6eeafb")
@@ -40,173 +49,97 @@ $("#searchSubmit").click(function() {
 
     .then(data => {
         console.log(data);
-        $.each(data.list[0], function() {
-            console.log($("dt"));
+
+        // creating a local variable for today's weather data within the object given back by the api
+        var today = data.list[0];
+        console.log(today);
+
+        // creating a card from the data in the today object
+        $('#searchResult').html(function() {
+            //convert the unix time to js time
+            var dt = new Date(today.dt * 1000); //timestamp * 1000 
+
+            return `<div class="col-auto display-inline-block" id="todayResult">
+                <div class="card col">
+                    <h2 class="card-title p-2">${data.city.name}</h2>
+                    <h5 class="card-title p-2">${dt.toDateString()}</h5>
+                    <div class="card-body">
+                        <img src="http://openweathermap.org/img/wn/${today.weather[0].icon}@2x.png"
+                            class="card-img-top"
+                            alt="${today.weather[0].description}"
+                        />
+                        <h3 class="card-title">${today.weather[0].main}</h3>
+                        <p class="card-text">
+                            High: ${today.main.temp_max}&deg;F Low: ${today.main.temp_min}&deg;F
+                        </p>
+                        <p class="card-text">
+                            Precipitation: ${today.pop * 100}%
+                        </p>
+                        <p class="card-text">
+                            Wind: ${today.wind.speed}mph
+                        </p>
+                    </div>
+                </div>
+            </div>`;
         });
 
+        return fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + data.city.coord.lat + "&lon=" + data.city.coord.lon + "&units=imperial&cnt=6&exclude=current,minutely,hourly,alert&appid=397ffb68060871749257fde6bd6eeafb");
     })
 
+    .then(function (response) {
+        if (!response.ok) {
+            throw response.json();
+        }
+        return response.json();
+    })
 
+    .then(data => {
+        console.log(data);
+
+        // need to clear the area first if there is data already there, becuase i use .append here, not .html
+        $('#searchForecast').empty()    ;
+
+        // now creating a for loop for the different days of the forecast
+        for (var i = 1; i < 6; i++) {
+            var today = data.daily[i];
+            console.log(today);
+    
+            // creating a card from the data in the today object
+            $('#searchForecast').append(function() {
+                //convert the unix time to js time
+                var dt = new Date(today.dt * 1000); //timestamp * 1000 
+                return `<div class="col display-inline-block" id="todayResult">
+                    <div class="card col">
+                        <h5 class="card-title p-2">${dt.toDateString()}</h5>
+                        <div class="card-body">
+                            <img src="http://openweathermap.org/img/wn/${today.weather[0].icon}@2x.png"
+                                class="card-img-top"
+                                alt="${today.weather[0].description}"
+                            />
+                            <h3 class="card-title">${today.weather[0].main}</h3>
+                            <p class="card-text">
+                                High: ${today.temp.max}&deg;F Low: ${today.temp.min}&deg;F
+                            </p>
+                            <p class="card-text">
+                                Precipitation: ${today.pop * 100}%
+                            </p>
+                            <p class="card-text">
+                                Wind: ${today.wind_speed}mph
+                            </p>
+                        </div>
+                    </div>
+                </div>`;
+            });
+        }
+    })
     .catch(console.error);
+};
 
+
+// // // annnnddd now im stuck here for now. buttons wont listen correctly and call the weather function
+
+//then add a listener to the past searches buttons that calls the findWeather
+$("li.btn").on("click", function() {
+    var cityZip = $(this).text();
+    findWeather(cityZip);
 });
-
-/*
-function getTodayWeather(xycords) {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + xycords[0] + "&lon=" + xycords[1] + "&units=imperial&appid=397ffb68060871749257fde6bd6eeafb")
-    //https://api.openweathermap.org/data/2.5/onecall?lat=" + xycords[0] + "&lon=" + xycords[1] + "&units=imperial&appid=397ffb68060871749257fde6bd6eeafb")
-    .then(function (response) {
-        if (!response.ok) {
-            throw response.json();
-        }
-        return response.json();
-    })
-
-    .then((data) => {
-        var i = 0;
-        for(var i = 0; i < data.length; i++) {
-            $('#searchResult').html(data.daily.map(function() {
-                var dt = new Date(day.dt * 1000); //timestamp * 1000
-        
-            //yeah this didnt work....
-            $('#searchResult').append("<div class='col-2 display-inline-block'><div class='card'><h5 class='card-title p-2'>${dt.toDateString()}</h5><div class='card-body'><img src='http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png'class='card-img-top'alt='${day.weather[0].description}'/><h3 class='card-title'>${day.weather[0].main}</h3><p class='card-text'>High: ${day.temp.max}&deg;F Low: ${day.temp.min}&deg;F</p><p class='card-text'>Precipitation: ${day.pop * 100}%</p><p class='card-text'>Wind: ${day.wind_speed}mph</p></div></div></div>");
-            }));
-        }
-    })
-    
-    .catch(console.error);
-}
-
-
-
-//start hereeeeeeeee // // // // trying to combine both prints into one api call
-
-
-//trying this for dealing with the response
-function createResult(response) {
-    console.log(response);
-    
-    //this will create todays weather
-    // not working correctly. need to try a different loop
-    /*
-    $('#searchResult').html(response.daily.map((day, index) => {
-        //index 0 means just 1 days data
-        if (index <= 4) {
-            //convert the unix time to js time
-            var dt = new Date(day.dt * 1000); //timestamp * 1000  // needs a variable for day.dt
-
-            return `<div class="col-2 display-inline-block">
-                <div class="card">
-                    <h5 class="card-title p-2">${dt.toDateString()}</h5>
-                    <div class="card-body">
-                        <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"    // needs a variable for day.weather[0].icon
-                            class="card-img-top"
-                            alt="${day.weather[0].description}"    // needs a variable for day.weather[0].description
-                        />
-                        <h3 class="card-title">${day.weather[0].main}</h3>    // needs a variable for day.weather[0].main
-                        <p class="card-text">
-                            High: ${day.temp.max}&deg;F Low: ${day.temp.min}&deg;F    // needss 2 variables day.temp.max and day.temp.min
-                        </p>
-                        <p class="card-text">
-                            Precipitation: ${day.pop * 100}%     // needs a variable for day.weather[0].main
-                        </p>
-                        <p class="card-text">
-                            Wind: ${day.wind_speed}mph
-                        </p>
-                    </div>
-                </div>
-            </div>`;
-        }
-        })
-    .join(' ')
-    );
-    
-        
-    console.log(response);
-    //this will create and print the 5 day forcast
-    $('#searchForecast').html(response.daily.map((day, index) => {
-        //index 4 means 5 days data
-        if (index <= 4) {
-            //convert the unix time to js time 
-            var dt = new Date(day.dt * 1000); //timestamp * 1000
-
-            return `<div class="col-3 display-inline-block">
-                <div class="card">
-                    <h5 class="card-title p-2">${dt.toDateString()}</h5>
-                    <div class="card-body">
-                        <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"
-                            class="card-img-top"
-                            alt="${day.weather[0].description}"
-                        />
-                        <h3 class="card-title">${day.weather[0].main}</h3>
-                        <p class="card-text">
-                            High: ${day.temp.max}&deg;F Low: ${day.temp.min}&deg;F
-                        </p>
-                        <p class="card-text">
-                            Precipitation: ${day.pop * 100}%
-                        </p>
-                        <p class="card-text">
-                            Wind: ${day.wind_speed}mph
-                        </p>
-                    </div>
-                </div>
-            </div>`;
-        }
-        })
-        .join(' ')
-    );
-    
-};
-/*
-function getForecastWeather(xycords) {
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + xycords[0] + "&lon=" + xycords[1] + "&units=imperial&appid=397ffb68060871749257fde6bd6eeafb")
-    .then(function (response) {
-        if (!response.ok) {
-            throw response.json();
-        }
-        return response.json();
-    })
-
-    .then((response) => {
-        createForecast(response);
-    })
-    
-    .catch(console.error);
-}
-
-//defining the function for the 5day forecast
-function createForecast(response) {
-    //this will create and print the 5 day forcast
-    $('#searchForecast').html(response.daily.map((day, index) => {
-        //index 4 means 5 days data
-        if (index <= 4) {
-            //convert the unix time to js time 
-            var dt = new Date(day.dt * 1000); //timestamp * 1000
-
-            return `<div class="col-2 display-inline-block">
-                <div class="card">
-                    <h5 class="card-title p-2">${dt.toDateString()}</h5>
-                    <div class="card-body">
-                        <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"
-                            class="card-img-top"
-                            alt="${day.weather[0].description}"
-                        />
-                        <h3 class="card-title">${day.weather[0].main}</h3>
-                        <p class="card-text">
-                            High: ${day.temp.max}&deg;F Low: ${day.temp.min}&deg;F
-                        </p>
-                        <p class="card-text">
-                            Precipitation: ${day.pop * 100}%
-                        </p>
-                        <p class="card-text">
-                            Wind: ${day.wind_speed}mph
-                        </p>
-                    </div>
-                </div>
-            </div>`;
-        }
-        })
-        .join(' ')
-    );
-};
-*/
