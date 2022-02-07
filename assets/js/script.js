7cff95b1a4bdcc7de2055076b6ad8d96f9dffc19
Kashane S <ksakhakorn@gmail.com>
2022-02-06 18:43:54 -0800

    getting stuck trying to figure out multiple api calls and then getting it to post the right data onto my website. taking a break. I had successful moments and was making it work with 1 api call.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,45 +7,130 @@ var xycords = [];
 //this is the url fetch function
 $("#searchSubmit").click(function() {
     cityZip = $("input").val();
-    xycords = getCityCoords(cityZip);
     console.log(xycords);
+    
+    //this function get the cords for each city
+    //trying a new api to get latlon
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+cityZip+"&limit=1&appid=397ffb68060871749257fde6bd6eeafb")
+    .then(response => {
+        // error checking the response
+        if (!response.ok) {
+            throw response;
+        }
+        return response.json();
+    })
 
-    //xycords = [41.8781, -87.6298];
-    //start hereeeeeeeee // // //
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + xycords[0] + "&lon=" + xycords[1] + "&units=imperial&appid=397ffb68060871749257fde6bd6eeafb")
+    .then(data => {
+        console.log(data);
+        for (const lat of data) {
+            xycords[0] = data[0].lat;
+        }
+        for (const lon of data) {
+            xycords[1] = data[0].lon;
+        }
+        return fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + xycords[0] + "&lon=" + xycords[1] + "&units=imperial&cnt=6&appid=397ffb68060871749257fde6bd6eeafb");
+    })
+    
     .then(function (response) {
         if (!response.ok) {
             throw response.json();
         }
-  
-        //console.log(response.json());
+        return response.json();
+    })
+
+    .then(data => {
+        console.log(data);
+        $.each(data.list[0], function() {
+            console.log($("dt"));
+        });
+
+    })
+
+
+    .catch(console.error);
+
+});
+
+/*
+function getTodayWeather(xycords) {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + xycords[0] + "&lon=" + xycords[1] + "&units=imperial&appid=397ffb68060871749257fde6bd6eeafb")
+    //https://api.openweathermap.org/data/2.5/onecall?lat=" + xycords[0] + "&lon=" + xycords[1] + "&units=imperial&appid=397ffb68060871749257fde6bd6eeafb")
+    .then(function (response) {
+        if (!response.ok) {
+            throw response.json();
+        }
         return response.json();
     })
 
     .then((data) => {
-        searchResult(data);
-    })
-
-    .then((data) => {
-        searchForecast(data);
+        var i = 0;
+        for(var i = 0; i < data.length; i++) {
+            $('#searchResult').html(data.daily.map(function() {
+                var dt = new Date(day.dt * 1000); //timestamp * 1000
+        
+            //yeah this didnt work....
+            $('#searchResult').append("<div class='col-2 display-inline-block'><div class='card'><h5 class='card-title p-2'>${dt.toDateString()}</h5><div class='card-body'><img src='http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png'class='card-img-top'alt='${day.weather[0].description}'/><h3 class='card-title'>${day.weather[0].main}</h3><p class='card-text'>High: ${day.temp.max}&deg;F Low: ${day.temp.min}&deg;F</p><p class='card-text'>Precipitation: ${day.pop * 100}%</p><p class='card-text'>Wind: ${day.wind_speed}mph</p></div></div></div>");
+            }));
+        }
     })
     
-    .catch(console.err);
-});
+    .catch(console.error);
+}
+
+
+
+//start hereeeeeeeee // // // // trying to combine both prints into one api call
+
 
 //trying this for dealing with the response
-function searchResult(response) {
+function createResult(response) {
     console.log(response);
+    
     //this will create todays weather
+    // not working correctly. need to try a different loop
+    /*
     $('#searchResult').html(response.daily.map((day, index) => {
         //index 0 means just 1 days data
-        if (index <= 0) {
+        if (index <= 4) {
             //convert the unix time to js time
-            var dt = new Date(day.dt * 1000); //timestamp * 1000
-            var sr = new Date(day.sunrise * 1000).toTimeString();
-            var ss = new Date(day.sunset * 1000).toTimeString();
+            var dt = new Date(day.dt * 1000); //timestamp * 1000  // needs a variable for day.dt
 
-            return `<div class="col-8 display-inline-block">
+            return `<div class="col-2 display-inline-block">
+                <div class="card">
+                    <h5 class="card-title p-2">${dt.toDateString()}</h5>
+                    <div class="card-body">
+                        <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"    // needs a variable for day.weather[0].icon
+                            class="card-img-top"
+                            alt="${day.weather[0].description}"    // needs a variable for day.weather[0].description
+                        />
+                        <h3 class="card-title">${day.weather[0].main}</h3>    // needs a variable for day.weather[0].main
+                        <p class="card-text">
+                            High: ${day.temp.max}&deg;F Low: ${day.temp.min}&deg;F    // needss 2 variables day.temp.max and day.temp.min
+                        </p>
+                        <p class="card-text">
+                            Precipitation: ${day.pop * 100}%     // needs a variable for day.weather[0].main
+                        </p>
+                        <p class="card-text">
+                            Wind: ${day.wind_speed}mph
+                        </p>
+                    </div>
+                </div>
+            </div>`;
+        }
+        })
+    .join(' ')
+    );
+    
+        
+    console.log(response);
+    //this will create and print the 5 day forcast
+    $('#searchForecast').html(response.daily.map((day, index) => {
+        //index 4 means 5 days data
+        if (index <= 4) {
+            //convert the unix time to js time 
+            var dt = new Date(day.dt * 1000); //timestamp * 1000
+
+            return `<div class="col-3 display-inline-block">
                 <div class="card">
                     <h5 class="card-title p-2">${dt.toDateString()}</h5>
                     <div class="card-body">
@@ -70,18 +155,33 @@ function searchResult(response) {
         })
         .join(' ')
     );
+    
 };
+/*
+function getForecastWeather(xycords) {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + xycords[0] + "&lon=" + xycords[1] + "&units=imperial&appid=397ffb68060871749257fde6bd6eeafb")
+    .then(function (response) {
+        if (!response.ok) {
+            throw response.json();
+        }
+        return response.json();
+    })
+
+    .then((response) => {
+        createForecast(response);
+    })
+    
+    .catch(console.error);
+}
 
 //defining the function for the 5day forecast
-function searchForecast(response) {
+function createForecast(response) {
     //this will create and print the 5 day forcast
     $('#searchForecast').html(response.daily.map((day, index) => {
         //index 4 means 5 days data
         if (index <= 4) {
-            //convert the unix time to js time
+            //convert the unix time to js time 
             var dt = new Date(day.dt * 1000); //timestamp * 1000
-            var sr = new Date(day.sunrise * 1000).toTimeString();
-            var ss = new Date(day.sunset * 1000).toTimeString();
 
             return `<div class="col-2 display-inline-block">
                 <div class="card">
@@ -101,12 +201,6 @@ function searchForecast(response) {
                         <p class="card-text">
                             Wind: ${day.wind_speed}mph
                         </p>
-                        <p class="card-text">
-                            Sunrise ${sr}
-                        </p>
-                        <p class="card-text">
-                            Sunset ${ss}
-                        </p>
                     </div>
                 </div>
             </div>`;
@@ -115,84 +209,4 @@ function searchForecast(response) {
         .join(' ')
     );
 };
-
-//this function get the cords for each city
-function getCityCoords(input) {
-    //trying a new api to get latlon
-    fetch("https://api.tomtom.com/search/2/structuredGeocode.json?key=3KGaX0rwxv1WUDYPM8eGAyyh01HbGcFC&countryCode=US&limit=1&municipality="+input)
-    .then(function(response) {
-        if (!response.ok) {
-            throw response;
-        }
-  
-        console.log(response);
-        return response;        
-    })
-
-    .then(function(data) {
-        searchLatLon(data);
-        return xycords;
-    })
-    
-    /*
-    //start testing for all lat and lon major cities
-    console.log(input);
-    if (input = "chicago"){
-        return [41.8781, -87.6298];
-    }
-    else if(input = "los angeles"){
-        return xycords = [34.0522, -118.2437];
-    }
-    else {
-        return xycords = "Sorry, this city is in the database."
-    }
-    */
-    return xycords;
-};
-
-function searchLatLon(response) {
-    var test = response.summary.map(geoBias, index) => {
-        
-        test[0] = geoBias.lat;
-        test[1] = geoBias.lon;
-    };
-    console.log(test);
-    /*
-    xycords[0] = response.summary.geoBase.lat;
-    xycords[1] = response.summary.geoBase.lon;
-    return xycords;
-    */
-}
-
-
-/*
-$("#pastSearches").
-
-
-will need to grab all lat and lon of major cities?
-or find an api that can convert city names to lat and lon
-
-function searchApi(latitude, longitude) {
-
-
-weather api:
-
-https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
-397ffb68060871749257fde6bd6eeafb
-
-https://api.openweathermap.org/data/2.5/onecall?city=chicago&appid=397ffb68060871749257fde6bd6eeafb
-
-*/
-
-
-/*
-now the api for lat and long geocoding
-
-"https://api.tomtom.com/search/2/structuredGeocode.json?key=3KGaX0rwxv1WUDYPM8eGAyyh01HbGcFC&countryCode=US&limit=1&municipality="+cityZip
-
-ill need to use results.position for latlon
-
-3KGaX0rwxv1WUDYPM8eGAyyh01HbGcFC
-
 */
