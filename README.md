07eb659bd0231138ef845d163ed28c5a5a50440c
Kashane S <ksakhakorn@gmail.com>
2022-02-09 15:43:42 -0800

    made the final changes and polished the code. finished the readme file and added the screenshot.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# weather-magic
-working with APIs to create a weather dashboard
+# Weather Magic
+
+[link to website](https://kashane1.github.io/weather-magic/)
+-----------
+Following the link above will bring you to the Weather Magic!
+
+-----------
+Requirements:
+-----------
+
+1. The user is presented with a weather dashboard with an input
+2. After they search for a city, they can see the weather for that day and a 5 day forecast
+3. The weather shows an icon of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+4. There is also a 5-day forecast with similar weather information
+5. The search history is also able to bring up past cities and their weather data
+
+
+-----------
+Process:
+-----------
+I enjoyed the process of this homework assignment because felt very close to what a real life project might require of me. I'm happy to be working with third party APIs even if they are a bit challenging at first. After some extra studying and a few youtube videos, I was able to get this API to do exactly what I wanted it to. The most challenging part was working with multiple API calls. I also made other small mistakes, typos really, that had me hung up for hours. It was a great practice in debugging and trying not to get too frustrated. 
+
+-----------
+```
+preview of Weather Magic!:
+```
+![Image](./assets/images/screenshot.jpg "screenshot of the weather magic")
+
+-----------
+Cited:
+-----------
+https://www.youtube.com/ - for API tutorials
+
+https://www.youtube.com/watch?v=WZNG8UomjSI&t=1428s - for more specific openweathermap API help
+
+https://api.jquery.com/ - for jquery references
+
+https://getbootstrap.com/docs/4.6/getting-started/introduction/ - for bootstrap documentation
+
+-----------
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,53 @@
-//creating global variables
+//creating global variables that are called in different functions
 var cityZip = "";
-var resultTextEl = document.createElement('p')
-var resultContentEl = document.createElement('p')
 var xycords = [];
 
 //needed to create a function to use for on click and for past search clicks
 //this selecter is only on the search input
 $("#searchSubmit").click(function() {
     cityZip = $("input").val();
-    console.log(xycords);
 
     // this creates the past searches list, event listener to research them is added later
-    $("#pastSearches").prepend("<li class='btn btn-info display-flex' id='"+cityZip+"'>"+cityZip+"</li>")
+    $("#pastSearches").prepend("<li class='btn btn-info display-flex m  -2 pastList'>"+cityZip+"</li>")
+    
+    //then add a listener to the past searches buttons that calls the findWeather. i first made the mistake of having this listener separated out
+    $("li.pastList").on("click", function() {
+        var pastCity = $(this).text();
 
+        // recalling the main function from the past searches element
+        findWeather(pastCity);
+    });
+
+    // calling the main function to fetch the APIs
     findWeather(cityZip);
 });
 
 //this is the weather function that can get called by the search input as well as the past search buttons
 function findWeather(cityZip) {
-    //this function get the cords for each city
-    //trying a new api to get latlon
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+cityZip+"&limit=1&appid=397ffb68060871749257fde6bd6eeafb")
+    // first fetch is used to just to get the coords for the other fetches
+    fetch("https://api.openweathermap.org/geo/1.0/direct?q="+cityZip+"&limit=1&appid=397ffb68060871749257fde6bd6eeafb")
     .then(response => {
         // error checking the response
         if (!response.ok) {
             throw response;
         }
+        // then returning the response in .json() format, which makes it an object
         return response.json();
     })
 
+    // .then will take the json object and pull information from it
     .then(data => {
-        console.log(data);
         for (const lat of data) {
             xycords[0] = data[0].lat;
         }
         for (const lon of data) {
             xycords[1] = data[0].lon;
         }
-        return fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + xycords[0] + "&lon=" + xycords[1] + "&units=imperial&cnt=6&appid=397ffb68060871749257fde6bd6eeafb");
+        // the return fetch was a big place i got hung up on, but finally found the answer online
+        return fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + xycords[0] + "&lon=" + xycords[1] + "&units=imperial&cnt=1&exclude=current,minutely,hourly,alert&appid=397ffb68060871749257fde6bd6eeafb");
     })
     
+    // always check the response is ok, and return it in .json() format
     .then(function (response) {
         if (!response.ok) {
             throw response.json();
@@ -47,81 +55,88 @@ function findWeather(cityZip) {
         return response.json();
     })
 
+    // with the data from the second fetch, i gather the weather data for today
     .then(data => {
-        console.log(data);
-
         // creating a local variable for today's weather data within the object given back by the api
-        var today = data.list[0];
-        console.log(today);
+        var today = data.daily[0];
+        /* sadly I was unable to get the UVI color changing to work but I will come back to this and try more later
+        var uvi = data.uvi
+
+            if (this.hmtl < 3){
+                $(this).attr("color","green");
+            }
+            else if (this.html < 8){
+                $(this).attr("color","yellow");
+            } 
+            else {
+                $(this).attr("color","red");
+            }
+        */
 
         // creating a card from the data in the today object
         $('#searchResult').html(function() {
             //convert the unix time to js time
-            var dt = new Date(today.dt * 1000); //timestamp * 1000 
+            var dt = new Date(today.dt * 1000);
 
+            // learned this return trick from stack overflow, the function will return all this html into the #searchResult
             return `<div class="col-auto display-inline-block" id="todayResult">
                 <div class="card col">
-                    <h2 class="card-title p-2">${data.city.name}</h2>
-                    <h5 class="card-title p-2">${dt.toDateString()}</h5>
-                    <div class="card-body">
-                        <img src="http://openweathermap.org/img/wn/${today.weather[0].icon}@2x.png"
+                    <h2 class="card-title p-2 text-center">${cityZip}</h2>
+                    <h5 class="card-title p-2 text-center">${dt.toDateString()}</h5>
+                    <div class="card-body text-center">
+                        <img src="https://openweathermap.org/img/wn/${today.weather[0].icon}@4x.png"
                             class="card-img-top"
                             alt="${today.weather[0].description}"
                         />
                         <h3 class="card-title">${today.weather[0].main}</h3>
                         <p class="card-text">
-                            High: ${today.main.temp_max}&deg;F Low: ${today.main.temp_min}&deg;F
+                            High: ${today.temp.max}&deg;F  Low: ${today.temp.min}&deg;F
                         </p>
                         <p class="card-text">
                             Precipitation: ${today.pop * 100}%
                         </p>
                         <p class="card-text">
-                            Wind: ${today.wind.speed}mph
+                            Humidity: ${today.humidity}%
+                        </p>
+                        <p class="card-text">
+                            Wind: ${today.wind_speed}mph
+                        </p>
+                        <p class="card-text">
+                            UV Index: ${today.uvi}
                         </p>
                     </div>
                 </div>
             </div>`;
-        });
-
-        return fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + data.city.coord.lat + "&lon=" + data.city.coord.lon + "&units=imperial&cnt=6&exclude=current,minutely,hourly,alert&appid=397ffb68060871749257fde6bd6eeafb");
-    })
-
-    .then(function (response) {
-        if (!response.ok) {
-            throw response.json();
-        }
-        return response.json();
+        }); 
+        return data;
     })
 
     .then(data => {
-        console.log(data);
-
         // need to clear the area first if there is data already there, becuase i use .append here, not .html
-        $('#searchForecast').empty()    ;
+        $('#searchForecast').empty();
 
         // now creating a for loop for the different days of the forecast
         for (var i = 1; i < 6; i++) {
             var today = data.daily[i];
-            console.log(today);
     
             // creating a card from the data in the today object
             $('#searchForecast').append(function() {
                 //convert the unix time to js time
                 var dt = new Date(today.dt * 1000); //timestamp * 1000 
                 return `<div class="col display-inline-block" id="todayResult">
-                    <div class="card col">
+                    <div class="card">
                         <h5 class="card-title p-2">${dt.toDateString()}</h5>
                         <div class="card-body">
-                            <img src="http://openweathermap.org/img/wn/${today.weather[0].icon}@2x.png"
+                            <img src="https://openweathermap.org/img/wn/${today.weather[0].icon}@2x.png"
                                 class="card-img-top"
                                 alt="${today.weather[0].description}"
                             />
                             <h3 class="card-title">${today.weather[0].main}</h3>
                             <p class="card-text">
-                                High: ${today.temp.max}&deg;F Low: ${today.temp.min}&deg;F
+                                High: ${today.temp.max}&deg;F  Low: ${today.temp.min}&deg;F
                             </p>
                             <p class="card-text">
-                                Precipitation: ${today.pop * 100}%
+                                Humidity: ${today.humidity}%
                             </p>
                             <p class="card-text">
                                 Wind: ${today.wind_speed}mph
@@ -129,17 +144,8 @@ function findWeather(cityZip) {
                         </div>
                     </div>
                 </div>`;
-            });
+            }); 
         }
     })
     .catch(console.error);
 };
-
-
-// // // annnnddd now im stuck here for now. buttons wont listen correctly and call the weather function
-
-//then add a listener to the past searches buttons that calls the findWeather
-$("li.btn").on("click", function() {
-    var cityZip = $(this).text();
-    findWeather(cityZip);
-});
